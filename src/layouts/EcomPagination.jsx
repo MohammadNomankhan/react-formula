c9f258398a28787022070dd6450a1d4b40b75585
MohammadNomankhan <khannoman1702@gmail.com>
2024-10-05 12:41:18 +0530

    feat: added new mini projects

--- src/layouts/EcomPagination.jsx
+++ src/layouts/EcomPagination.jsx
@@ -6,18 +6,18 @@ const PAGE_SIZE = 9;
 
 const EcomPagination = () => {
     const [pageIdx, setPageIdx] = useState(0);
-    const pages = Array.from({length: Math.ceil(products.length / PAGE_SIZE)}, (_, idx) => idx+1);
+    const pages = Array.from({ length: Math.ceil(products.length / PAGE_SIZE) }, (_, idx) => idx + 1);
 
     return <div className="flex flex-col justify-center items-center py-60 bg-stone-100 px-4">
         <div className="w-full max-w-2xl">
             <div className="mb-8 pb-2 border-b-2 border-gray-200 flex gap-12 flex-col">
                 <h2 className="text-3xl font-medium text-slate-600 text-center">Footpath's big billion days</h2>
                 <div className="self-end flex gap-2">
-                    {pages.map((page, idx) => <button key={idx} onClick={() => setPageIdx(idx)} className={clsx("w-4 text-sm rounded-sm border", pageIdx == idx ? 'bg-rose-100 border-rose-500': 'bg-gray-300 border-gray-500')}>{page}</button>)}
+                    {pages.map((page, idx) => <button key={idx} onClick={() => setPageIdx(idx)} className={clsx("w-4 text-sm rounded-sm border", pageIdx == idx ? 'bg-rose-100 border-rose-500' : 'bg-gray-300 border-gray-500')}>{page}</button>)}
                 </div>
             </div>
             <div>
-                {products.slice(9 * (pageIdx), 9 * (pageIdx + 1)).map(product => <ProductCard key={product.id} product={product}/>)}
+                {products.slice(9 * (pageIdx), 9 * (pageIdx + 1)).map(product => <ProductCard key={product.id} product={product} />)}
             </div>
         </div>
     </div>
